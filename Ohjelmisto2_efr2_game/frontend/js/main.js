e08edbc2f535c4ea181cb5f09748ed942faefd95
Pavelishko <pdegterev@gmail.com>
2023-12-08 23:29:31 +0200

    Changes to familiarize myself with the project. # own css file for story htm # 2 dialog windows # design changes according to Specification document
    Plan for tomorrow: get the map working

--- Ohjelmisto2_efr2_game/frontend/js/main.js
+++ Ohjelmisto2_efr2_game/frontend/js/main.js
@@ -3,3 +3,31 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+/*  help menu  */
+
+const story = document.querySelector('#story')
+const manual = document.querySelector('#manual')
+const dialog = document.querySelectorAll('.help_menu')
+const closeStory = document.querySelector('#close_story')
+const closeLeManuelle = document.querySelector('#close_manual')
+
+story.addEventListener('click', () => {
+    console.log(dialog[0])
+    dialog[0].showModal();
+});
+
+manual.addEventListener('click', () => {
+    dialog[1].showModal();
+});
+
+closeStory.addEventListener('click', () => {
+    console.log('click')
+    dialog[0].close();
+});
+
+closeLeManuelle.addEventListener('click', () => {
+    console.log('click')
+    dialog[1].close();
+});
