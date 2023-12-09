1837491d17aa6a7e020797c8c9829101f0ef2ed6
Pavelishko <pdegterev@gmail.com>
2023-12-09 18:34:00 +0200

    Leaflet upgrade
    # It works with fresh created svg
    # All station locations in main.js

--- Ohjelmisto2_efr2_game/frontend/js/main.js
+++ Ohjelmisto2_efr2_game/frontend/js/main.js
@@ -1,8 +1,67 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// map initialization
+const map = L.map('map').setView([63, 43], 3.8)  // ([lat "korkeus", long "leveys"], zoom)
+
+// svg layer
+
+var imageUrl = '../img/Russia_Rail_Map.svg',
+    imageBounds = [
+        [0, 0],
+        [100, 100]
+    ];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+
+/*   markers   */
+
+var SaintPeterburg = L.marker([67.39868, 15.3]).addTo(map);
+var Murmansk = L.marker([70.15467, 25.73]).addTo(map);
+var Arkhangelsk = L.marker([67.444719, 24.4]).addTo(map);
+var Pechora = L.marker([65.783985, 32.56]).addTo(map);
+var Vorkuta = L.marker([66.40648, 37.276027]).addTo(map);
+var Yaroslavl = L.marker([64.852794, 18.2]).addTo(map);
+var Moscow = L.marker([64.53859, 14.9]).addTo(map);
+var Voronezh = L.marker([62.348729, 13.1]).addTo(map);
+var Krasnodar = L.marker([59.235846, 6.5]).addTo(map);
+var Volgograd = L.marker([59.326614, 13.35]).addTo(map);
+var Astrakhan = L.marker([57.175875, 14.2]).addTo(map);
+var Kazan = L.marker([62.183766, 21]).addTo(map);
+var Perm = L.marker([62.216313, 27.36]).addTo(map);
+var Yekaterinburg = L.marker([60.763599, 29.65]).addTo(map);
+var Saratov = L.marker([60.4, 16.5]).addTo(map);
+var Ufa = L.marker([60.165642, 25.706749]).addTo(map);
+var Kurgan = L.marker([59.477617, 31.95]).addTo(map);
+var Orenburg = L.marker([58.840288, 23]).addTo(map);
+var Orsk = L.marker([57.813579, 25.57]).addTo(map);
+var Tyumen = L.marker([60.336036, 33.3]).addTo(map);
+var Surgut = L.marker([62.189449, 40]).addTo(map);
+var NovyUrengoy = L.marker([64.631792, 43.9]).addTo(map);
+var Omsk = L.marker([58.070165, 38.05]).addTo(map);
+var Krasnoyarsk = L.marker([58.081783, 52.8]).addTo(map);
+var Bratsk = L.marker([58.509039, 58.65]).addTo(map);
+var Irkutsk = L.marker([55.811589, 61.73]).addTo(map);
+var Chita = L.marker([56.739684, 69.32]).addTo(map);
+var Tynda = L.marker([60.773494, 75.5]).addTo(map);
+var Tommot = L.marker([62.384446, 73.6]).addTo(map);
+var UstIlimsk = L.marker([59.859554, 59.2]).addTo(map);
+var Urgal = L.marker([59.95, 83.7]).addTo(map);
+var Khabarovsk = L.marker([59.125561, 87.7]).addTo(map);
+var Vladivostok = L.marker([55.074125, 88.921864]).addTo(map);
+
+
+
+/*    location by click    */
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
 
 /*   Restart   */
 
@@ -22,7 +81,6 @@ const restart = document.querySelector('#restart')
 restart.addEventListener('click', () => {
     location.reload()
 });
-
 
 
 
